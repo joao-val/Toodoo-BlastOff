a36a824559997102c0ccc25a15495a5442646521
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-04-14 11:53:26 -0300

    Tarefa-03

---
+++ C# Fundamentals/Tarefas/Tarefa_03/Tarefa_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangle triangle = new Triangle();
+
+            triangle.test();
+        }
+    }
+}
---
+++ C# Fundamentals/Tarefas/Tarefa_03/Tarefa_03/Service/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa_03
+{
+    internal class Triangle
+    {
+
+        public Triangle()
+        {
+        }
+
+        public void test()
+        {
+            var test1 = false;
+            var test2 = false;
+            var test3 = false;
+            var testNumber = false;
+            string A, B, C;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the value in cm of 3 lines (A, B, C) to check if they can form a triangle.");
+                Console.WriteLine("|B - C| < A < B+C \n|A - C| < B < A+C \n|A - B| < C < A+B\n");
+                Console.WriteLine("First line A:");
+                A = Console.ReadLine();
+
+                Console.WriteLine("Second line B:");
+                B = Console.ReadLine();
+
+                Console.WriteLine("Third line C:");
+                C = Console.ReadLine();
+
+                testNumber = (double.TryParse(A, out double i) && double.TryParse(B, out i) && double.TryParse(C, out i));
+            } while (testNumber == false);
+
+            var a = double.Parse(A);
+            var b = double.Parse(B);
+            var c = double.Parse(C);
+
+            test1 = (b - c) < a && a < (b + c);
+            test2 = (a - c) < b && b < (a + c);
+            test3 = (a - b) < c && c < (a + b);
+            
+            if ((test1 && test2 && test3) == true)
+            {
+                Console.Clear();
+                Console.WriteLine("These 3 lines can form a triangle!!");
+                Console.WriteLine(A + "cm|" + B + "cm|" + C + "cm");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Error, these 3 lines cannot form a triangle!!");
+                Console.WriteLine(A + "cm|" + B + "cm|" + C + "cm");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
