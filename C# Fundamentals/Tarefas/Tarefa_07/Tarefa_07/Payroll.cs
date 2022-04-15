2e0730023320bf4e39bebc7c77e02f239e15ca2c
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-04-15 20:11:04 -0300

    Tarefa - 07 - Fundamentos do C#

---
+++ C# Fundamentals/Tarefas/Tarefa_07/Tarefa_07/Payroll.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa_07
+{
+    public class Payroll
+    {
+        public Payroll()
+        {
+        }
+
+        public void sellers()
+        {
+            var sales = new string[10];
+            var total = new double[10];
+            var baseSalary = 200;
+            var position = new double[10];
+            bool test;
+
+            Console.Clear();
+            Console.WriteLine("Enter the gross sales amount in $ of 10 sellers.");
+            Console.WriteLine("The salesperson receives 9% of the gross value plus $200 base salary from his sales...\n");
+            for (int i = 0; i < 10; i++)
+            {
+                test = false;
+                
+                Console.WriteLine("Enter the gross sales amount for seller " + (i + 1) + ":");
+                sales[i] = Console.ReadLine();
+                test = double.TryParse(sales[i], out double x);
+
+                if(test == false)
+                {
+                    Console.WriteLine("Incorrect format!");
+                    Console.ReadKey();
+                    sellers();
+                }
+
+                total[i] = baseSalary + (double.Parse(sales[i]) * 0.09);
+                
+                if (total[i] > 1000)  position[8] += 1;
+            }
+            Console.Clear();
+
+            var range1 = Enumerable.Range(200, 799);
+            int[] Range = Enumerable.ToArray(range1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (var r in Range)
+                {
+                    var result = r;
+                    if (Math.Ceiling(total[i]) == result)
+                    {
+                        Console.WriteLine(r);
+                        result = (r - 200) / 100;
+                        position[result] += 1;
+                    }
+                    
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(position[i]+ " sellers" + " $" + baseSalary + " - $" + (baseSalary + 99));
+                baseSalary = baseSalary + 100;
+            }
+            Console.WriteLine(position[8] + " $" + baseSalary + " onward");
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ C# Fundamentals/Tarefas/Tarefa_07/Tarefa_07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tarefa_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Payroll payroll = new Payroll();
+            payroll.sellers();
+        }
+    }
+}
