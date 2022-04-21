94cd52e11bea7c97350259cca83b850cd0891f0b
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-04-21 03:38:00 -0300

    Tarefa-01-POO

---
+++ POO/Tarefas_POO_Balta/Tarefa-01-POO/Tarefa-01-POO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Tarefa_01_POO.entities;
+
+namespace Tarefa_01_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                var rectangle = new Rectangle();
+                var test = false;
+                string length, width;
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter the base size in 'm' (meters):");
+                    length = Console.ReadLine();
+                    Console.WriteLine("Enter the height size in 'm' (meters):");
+                    width = Console.ReadLine();
+
+                    test = (double.TryParse(length, out double x) && double.Parse(length) > 0)
+                        && (double.TryParse(width, out x) && double.Parse(width) > 0);
+                    
+                    if (test == false)
+                    {
+                        Console.WriteLine("Please enter the values in the correct format!");
+                        Console.ReadKey();
+                    }
+                    } while (test == false);
+
+                rectangle.Length = double.Parse(length);
+                rectangle.Width = double.Parse(width);
+
+                Console.Clear();
+                rectangle.PrintValueOfSides();
+                Console.WriteLine("------------------------");
+
+                Console.WriteLine("Do you want to change the value of the sides?");
+                Console.WriteLine("Enter -1 to yes(any other key to continue):");
+                if (Console.ReadLine() == "-1")
+                {
+                    rectangle.ChangeValueOfSides();
+                    Console.WriteLine("------------------------");
+                    rectangle.PrintValueOfSides();
+                }
+
+                Console.Clear();
+                Console.WriteLine($"{rectangle.CalculateArea()}m² of flooring and {rectangle.CalculatePerimeter()}m of baseboards will be needed to serve the place.");
+                Console.WriteLine("Type -1 to exit");
+            }while (Console.ReadLine() != "-1");
+        }
+    }
+}
---
+++ POO/Tarefas_POO_Balta/Tarefa-01-POO/Tarefa-01-POO/entities/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Tarefa_01_POO.entities
+{
+    public class Rectangle
+    {
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public void ChangeValueOfSides()
+        {
+            Console.Clear();
+            var test = false;
+            Console.WriteLine("Enter the new value for base:");
+            var newLength = Console.ReadLine();
+            
+            Console.WriteLine("Enter the new value for height:");
+            var newWidth = Console.ReadLine();
+
+            test = (double.TryParse(newLength, out double x) && double.Parse(newLength) > 0)
+                && (double.TryParse(newWidth, out  x) && double.Parse(newWidth) > 0);
+
+            if (test == false)
+            {
+                Console.WriteLine("Please enter the values in the correct format!");
+                Console.ReadKey();
+                ChangeValueOfSides();
+            }
+
+            Length = double.Parse(newLength);
+            Width = double.Parse(newWidth);
+        }
+
+        public void PrintValueOfSides()
+        {
+            Console.WriteLine($" Base = {Length}m | Height = {Width}m");
+        }
+
+        public double CalculateArea()
+        {
+            var area = Length * Width;
+            return area;
+        }
+
+        public double CalculatePerimeter()
+        {
+            var perimeter = 2*(Length + Width);
+            return perimeter;
+        }
+
+        public double Width { get; set; }
+
+        public double Length { get; set; }
+
+    }
+}
