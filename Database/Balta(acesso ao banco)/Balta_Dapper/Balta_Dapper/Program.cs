6788afce9b221ad7fdf4c773615a49855d3d5c43
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-06-14 15:07:15 -0300

    Acesso a dados balta (Imersão) Transactions

--- Database/Balta(acesso ao banco)/Balta_Dapper/Balta_Dapper/Program.cs
+++ Database/Balta(acesso ao banco)/Balta_Dapper/Balta_Dapper/Program.cs
@@ -23,7 +23,8 @@ using (var connection = new SqlConnection(connectionString))
     //OneToMany(connection);
     //QueryMultiple(connection);
     //SelectIn(connection);
-    Like(connection, "backend");
+    //Like(connection, "backend");
+    Transaction(connection);
 }
 
 
@@ -348,4 +349,49 @@ static void Like(SqlConnection connection, string term)
     {
         Console.WriteLine(item.Title);
     }
+}
+
+static void Transaction(SqlConnection connection)
+{
+    var category = new Category();
+    category.Id = Guid.NewGuid();
+    category.Title = "Minha categoria que não quero";
+    category.Url = "amazon";
+    category.Description = "Categoria destinada a serviços do AWS";
+    category.Order = 8;
+    category.Summary = "AWS Cloud";
+    category.Featured = false;
+
+    // SQL injection 'INSERT INTO Users VALUES()'
+
+    var insertSql = $@"INSERT INTO 
+        [Category] 
+    VALUES(
+        @Id, 
+        @Title, 
+        @Url, 
+        @Summary, 
+        @Order, 
+        @Description, 
+        @Featured)";
+
+    connection.Open();
+    using(var transaction = connection.BeginTransaction())
+    {
+        var rows = connection.Execute(insertSql, new
+        {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        }, transaction);
+        transaction.Commit();
+        //transaction.Rollback();
+
+        Console.WriteLine($"{rows} linhas inseridas");
+    }
+    
 }
