274b397c7dea2187c4da4720787bcb0f900af66f
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-06-14 14:48:17 -0300

    Acesso a dados balta (Imersão) QueryMultiple

--- Database/Balta(acesso ao banco)/Balta_Dapper/Balta_Dapper/Program.cs
+++ Database/Balta(acesso ao banco)/Balta_Dapper/Balta_Dapper/Program.cs
@@ -20,7 +20,8 @@ using (var connection = new SqlConnection(connectionString))
     //ExecuteScalar(connection);
     //ReadView(connection);
     //OneToOne(connection);
-    OneToMany(connection);
+    //OneToMany(connection);
+    QueryMultiple(connection);
 }
 
 
@@ -288,6 +289,27 @@ static void OneToMany(SqlConnection connection)
         foreach (var item in career.Items)
         {
             Console.WriteLine($" - {item.Title} ");
+        }
+    }
+}
+
+static void QueryMultiple(SqlConnection connection)
+{
+    var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+    using(var multi = connection.QueryMultiple(query))
+    {
+        var categories = multi.Read<Category>();
+        var courses = multi.Read<Course>();
+
+        foreach (var item in categories)
+        {
+            Console.WriteLine(item.Title);
+        }
+
+        foreach (var item in courses)
+        {
+            Console.WriteLine(item.Title);
         }
     }
 }
