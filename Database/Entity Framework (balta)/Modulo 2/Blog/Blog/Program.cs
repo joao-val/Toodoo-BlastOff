cb56f8bba93a72eb50c8c6bfc46c2ff916819e40
Joao Vitor Alves Lima <joao.vitor@toodoo.com.br>
2022-06-18 01:04:19 -0300

    Fundamentos do entity framework (balta) Include

--- Database/Entity Framework (balta)/Modulo 2/Blog/Blog/Program.cs
+++ Database/Entity Framework (balta)/Modulo 2/Blog/Blog/Program.cs
@@ -8,34 +8,46 @@ Console.WriteLine("Hello, World!");
 
 using var context = new BlogDataContext();
 
-var user = new User
+//var user = new User
+//{
+//    Name = "André Baltieri",
+//    Slug = "andrebaltieri",
+//    Email = "andre@balta.io",
+//    Bio = "9x Microsoft MVP",
+//    Image = "https://balta.io",
+//    PasswordHash = "123098457"
+//};
+
+//var category = new Category
+//{ 
+//    Name = "Backend",
+//    Slug = "backend"
+//};
+
+//// SCOPE_IDENTITY
+//var post = new Post
+//{
+//    Author = user,
+//    Category = category,
+//    Body = "<p>Hello world</p>",
+//    Slug = "comecando-com-ef-core",
+//    Summary = "Neste artigo vamos aprender EF core",
+//    Title = "Começando com EF core",
+//    CreateDate = DateTime.Now,
+//    LastUpdateDate = DateTime.Now,
+//};
+
+//context.Posts.Add(post);
+//context.SaveChanges();
+
+var posts = context
+    .Posts
+    .AsNoTracking()
+    .Include(x => x.Author)
+    .OrderByDescending(x => x.LastUpdateDate)
+    .ToList();
+
+foreach (var post in posts)
 {
-    Name = "André Baltieri",
-    Slug = "andrebaltieri",
-    Email = "andre@balta.io",
-    Bio = "9x Microsoft MVP",
-    Image = "https://balta.io",
-    PasswordHash = "123098457"
-};
-
-var category = new Category
-{ 
-    Name = "Backend",
-    Slug = "backend"
-};
-
-// SCOPE_IDENTITY
-var post = new Post
-{
-    Author = user,
-    Category = category,
-    Body = "<p>Hello world</p>",
-    Slug = "comecando-com-ef-core",
-    Summary = "Neste artigo vamos aprender EF core",
-    Title = "Começando com EF core",
-    CreateDate = DateTime.Now,
-    LastUpdateDate = DateTime.Now,
-};
-
-context.Posts.Add(post);
-context.SaveChanges();
+    Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+}
